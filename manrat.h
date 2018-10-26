3c03603adb95eaf0dc427e7338182216af8759ad
Zundel Stéphane <zundel.stephane@sfr.fr>
2018-10-26 14:41:28 +0200

    Mise en commentaire des prototypes de fonctions (inutiles)

--- manrat.h
+++ manrat.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/*
 float somme(float a, float b);
 float difference(float a, float b);
 float produit (float a, float b);
@@ -9,7 +11,7 @@ float factorielle (int n);
 float puissance (float a, float b);
 float racine_carre (float a);
 float racine_nieme (float a, float b);
-
+*/
 
 float somme(float a, float b) {		// Fonction relative à la somme.
     float r;
